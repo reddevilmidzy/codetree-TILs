2d56e5b35c09d18626ed24c154580720e257ff0f
CHOI DONGGEUN <78539407+reddevilmidzy@users.noreply.github.com>
2023-11-28 15:37:57 +0900

    Title: 글자 섞기, Time: 439ms, Memory: 31MB, Status: Passed - CodeTree

--- 231128/글자 섞기/shuffle-letters.py
+++ 231128/글자 섞기/shuffle-letters.py
@@ -3,29 +3,20 @@ import sys
 input = sys.stdin.readline
 
 n = int(input())
-words = []
+words = [''.join(sorted(input().rstrip())) for _ in range(n)]
 fore = []
 back = []
+for word in words:
+    fore.append(word)
+    back.append(word[::-1])
 
-for i in range(n):
-    s = input().rstrip()
-    ss = "".join(sorted(s))
-    words.append(ss)
-    # fore.append((ss, i))
-    # back.append((ss[::-1], i))
-    fore.append(ss)
-    back.append(ss[::-1])
-fore.sort(key=lambda x:x[0])
-back.sort(key=lambda x:x[0])
-
+fore.sort()
+back.sort()
 
 for word in words:
-    l = bisect_left(back, word)
-
-    print(l+1,end=' ')
-    val = bisect_left(fore, word[::-1])
-    r = bisect_right(fore, word[::-1])
-    if val+1!=r:
-        print(r)
-    else:
-        print(val + 1)
+    # l = bisect_left(fore, word[::-1])
+    # r = bisect_right(fore, word[::-1])
+    # print("l",l,"r",r)
+    print(bisect_left(back, word)+1, bisect_right(fore, word[::-1]))
+# print(fore)
+# print(back)
