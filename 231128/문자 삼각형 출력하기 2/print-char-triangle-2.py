a24da0c28ee49e4861630c522b1b3e01010d30ed
CHOI DONGGEUN <78539407+reddevilmidzy@users.noreply.github.com>
2023-11-28 15:54:45 +0900

    Title: 문자 삼각형 출력하기 2, Time: 132ms, Memory: 25MB, Status: Passed - CodeTree

--- 231128/문자 삼각형 출력하기 2/print-char-triangle-2.py
+++ 231128/문자 삼각형 출력하기 2/print-char-triangle-2.py
@@ -1,9 +1,13 @@
+alp = lambda x: chr(x%26+65)
 n = int(input())
 m = (n+1)//2
 
 arr = [['']*m for _ in range(n)]
-
+cnt = 0
 for j in range(m-1, -1, -1):
-    print("j",j)
-    for i in range(m-j, n, 1):
-        print(i,j)
+    for i in range(j, n-j, 1):
+        arr[i][j] = alp(cnt)
+        cnt += 1
+
+for i in arr:
+    print(*i)
