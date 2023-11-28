11138275371953ab9625c911f83c176ba04d91b9
CHOI DONGGEUN <78539407+reddevilmidzy@users.noreply.github.com>
2023-11-28 23:27:07 +0900

    Title: 효율적으로 분배하기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 231128/효율적으로 분배하기/to-distribute-efficiently.py
@@ -0,0 +1,10 @@
+INF = int(1e9)
+n = int(input())
+dp = [INF]*(n+1)
+dp[3] = 1
+dp[5] = 1
+
+for i in range(6, n+1):
+    dp[i] = min(dp[i-3] + 1, dp[i-5] + 1, dp[i])
+
+print(dp[n] if dp[n] != INF else -1)
